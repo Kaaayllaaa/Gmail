44e5f098133aab91147afaed4a583a1d906004f8
kayla <kaylatoyewole@gmail.com>
2023-05-05 21:49:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/gmail/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.gmail
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var emails: MutableList<Email>
+    private lateinit var adapter: EmailAdapter
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.loadMoreBtn).setOnClickListener {
+            // Fetch next 5 emails
+            val newEmails = EmailFetcher.getNext5Emails()
+            // Add new emails to existing list of emails
+            emails.addAll(newEmails)
+            // Notify the adapter there's new emails so the RecyclerView layout is updated
+            adapter.notifyDataSetChanged()
+        }
+
+        // Lookup the RecyclerView in activity layout
+        val emailsRv = findViewById<RecyclerView>(R.id.emailsRv)
+        // Fetch the list of emails
+        emails = EmailFetcher.getEmails()
+        // Create adapter passing in the list of emails
+        val adapter = EmailAdapter(emails)
+        // Attach the adapter to the RecyclerView to populate items
+        emailsRv.adapter = adapter
+        // Set layout manager to position the items
+        emailsRv.layoutManager = LinearLayoutManager(this)
+
+    }
+}
